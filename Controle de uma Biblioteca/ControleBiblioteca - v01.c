b3af2d9bcba1d89f3991fc530942f047ff108e33
Marcello <marcello_doalves@hotmail.com>
2017-04-05 11:32:23 -0300

    Menu funcionário simples

--- Controle de uma Biblioteca/ControleBiblioteca - v01.c
+++ Controle de uma Biblioteca/ControleBiblioteca - v01.c
@@ -39,6 +39,8 @@ typedef struct {
 	char nome[15];
 	short sexo; // 0-mulher 1-homem
 	char dataNasc[10];
+	char login[10];
+	char senha[10];
 	
 } pessoa;
 
@@ -80,14 +82,13 @@ typedef struct {
 	int idAluno;
 	int idFunc;
 
-	
  } emprestimo;
 
 typedef struct {
 	
 	pessoa estudante;
 	int matricula;
-	short sitMatr; // 0-trancada 1-ativa 2-jubilada
+	short sitMatr; // 1 - Ativa 2 - Bloqueada 3 - Debito
 	char turma;
 	endereco end;
 	telefone tel;
@@ -96,9 +97,7 @@ typedef struct {
 
 typedef struct {
 	
-	short idAdm; // 0-funcionario 1-administrador
-	char login[10];
-	char senha[10];
+	short idAdm; // 1-funcionario 2-administrador
 	endereco end;
 	telefone tel;
 	pessoa func;
@@ -177,6 +176,39 @@ void criarListaEmp(empEST *E) {
 }
 
 
+int verificarListaVaziaFunc (funcEST F) {
+	return(F.Prim == F.Ult); // Quando Prim for igual a Ult a lista está vazia
+}
+int verificarListaVaziaAlun (alunoEST A) {
+	return(A.Prim == A.Ult); // Quando Prim for igual a Ult a lista está vazia
+}
+int verificarListaVaziaEdit (editEST ED) {
+	return(ED.Prim == ED.Ult); // Quando Prim for igual a Ult a lista está vazia
+}
+int verificarListaVaziaLivro (livroEST L) {
+	return(L.Prim == L.Ult); // Quando Prim for igual a Ult a lista está vazia
+}
+int verificarListaVaziaEmp (empEST E) {
+	return(E.Prim == E.Ult); // Quando Prim for igual a Ult a lista está vazia
+}
+
+int verificarListaCheiaFunc (funcEST F) {
+	return(F.Ult == qFunc); // Quando Ult apontar para a posição Max a lista está cheia
+}
+int verificarListaCheiaAluno (alunoEST A) {
+	return(A.Ult == qAlunos); // Quando Ult apontar para a posição Max a lista está cheia
+}
+int verificarListaCheiaEdit (editEST ED) {
+	return(ED.Ult == qEditoras); // Quando Ult apontar para a posição Max a lista está cheia
+}
+int verificarListaCheiaLivro (livroEST L) {
+	return(L.Ult == qLivros); // Quando Ult apontar para a posição Max a lista está cheia
+}
+int verificarListaCheiaEmp (empEST E) {
+	return(E.Ult == qEmprestimos); // Quando Ult apontar para a posição Max a lista está cheia
+}
+
+
 
 
 
@@ -184,19 +216,316 @@ void criarListaEmp(empEST *E) {
 
 void menu () {
 	
-	//Criar listas estáticas
+	do { //Responsável por terminar o programa
+	
+	funcEST F;
+	alunoEST A;
+	editEST ED;
+	livroEST L;
+	empEST E;
+	
+	criarListaFunc(&F);
+	criarListaAluno(&A);
+	criarListaEditora(&ED);
+	criarListaLivro(&L);
+	criarListaEmp(&E);
+	
+	//carregarArquivosFunc(&F);
+	//carregarArquivosAluno(&A);
+	//carregarArquivosEdit(&ED);
+	//carregarArquivosLivro(&L);
+	//carregarArquivosEmp(&E);
+	
+	printf ("\n\n\tRecomendamos que use a tela em tamanho máximo para mais conforto.");
+	sleep(3);
 
+		int login;
+		int i; //Usado para os laços de repetição sem comprometimento e necessidade posterior de
+			   //uma posição qualquer da contagem
+		do {
+			system ("cls");
+			printf ("\n\t\t\tLOGIN\n\n\n");
+			printf("\n\t1 - Funcionário");
+			printf("\n\t2 - Aluno");
+			printf("\n\t3 - Sair");
+			printf("\n\tOpcão: ");
+			scanf("%d", &login);
+			fflush(stdin);
+			
+			short saidaLogin = 0;
+			char loginErro = 0;
+			short menuP = 0;
+			short menuS = 0;
+			if (login == 1) {
+			
+				char login[10], senha[10];
+				
+				do {
+				saidaLogin = 0;
+					
+					printf ("\n\t\t\tLOGIN\n\n\n");
+					printf ("\n\tUser: ");
+					scanf ("%s", &login);
+					fflush(stdin);
+					printf("\n\tSenha: ");
+					scanf ("%s", &senha);
+					fflush(stdin);
+					
+					for (i = 0; i < 50; i++) {
+						
+						if (strcmp(F.F[i].func.login,login) == 0 && strcmp(F.F[i].func.senha,senha) == 0 &&
+										F.F[i].idAdm == 1) {
+							
+							printf ("\n\n\tLogin realizado com sucesso!");
+							saidaLogin = 1;
+							
+						} 
+						
+							if (strcmp(F.F[i].func.login,login) == 0 && strcmp(F.F[i].func.senha,senha) == 0 &&
+										F.F[i].idAdm == 2) {
+							
+							printf ("\n\n\tLogin realizado com sucesso!");
+							saidaLogin = 3;
+							
+						} 
+						
+					}
+					
+					if (saidaLogin == 0) {
+						
+						short aux = 0;
+						do {
+							
+							system ("cls");
+							printf ("\n\t\t\tLOGIN\n\n\n");
+							printf ("\n\n\tSenha e/ou usuário está(ão) errado(s).");
+							printf("\n\tDeseja tentar novamente?");
+							printf("\n\tS - Sim, parece ótimo.");
+							printf("\n\tN - Não, sair.");
+							scanf ("%c", &loginErro);
+						
+							if (loginErro == 's' || loginErro =='S') {
+								
+								aux = 1;
+
+							} else if (loginErro == 'n' || loginErro =='N') {
+								
+								aux = 2;
+								saidaLogin = 2;
+								
+							}
+
+							
+						} while(aux != 1 && aux != 2);
+						
+					}
+					
+					
+				} while (saidaLogin != 1 && saidaLogin != 2 && saidaLogin != 3);
+				
+				
+				if (saidaLogin == 1) { //Menu para Funcionario comum
+					
+					
+					do {
+					
+						system("cls");
+						printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+						
+						printf("\n\tSub Menus: ");
+						printf("\n\t1 - Livros");
+						printf("\n\t2 - Editoras");
+						printf("\n\t3 - Alunos");
+						printf("\n\t4 - Emprestimos e Devoluções");
+						printf("\n\t5 - Relatórios");
+						printf("\n\t6 - Sair");
+						printf("\n\tOpcão: ");	
+						scanf("%d", &menuP);
+						fflush(stdin);
+						menuS = 0;
+						
+						if (menuP == 1) {
+							
+							do {
+								
+								system("cls");
+								printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+								printf("\n\tSub Menu -> Livros");
+								
+								printf("\n\t1 - Consultar Livro");
+								printf("\n\t2 - Cadastrar Livro");
+								printf("\n\t3 - Editar Livro");
+								printf("\n\t4 - Excluir Livro");
+								printf("\n\t5 - Livros com maior procura");
+								printf("\n\t6 - Livros com menor procura");
+								printf("\n\t7 - Sair");
+								printf("\n\tOpcão: ");	
+								scanf("%d", &menuS);
+								fflush(stdin);
+								
+								if(menuS == 1) {
+									
+								} else if (menuS == 2){
+								
+								} else if (menuS == 3) {
+									
+								} else if (menuS == 4) {
+									
+								} else if (menuS == 5) {
+									
+								} else if (menuS == 6) {
+									
+								}
+								
+								
+							} while (menuS != 7);
+							
+							
+						} else if (menuP == 2) {
+							
+							do {
+								
+								system("cls");
+								printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+								printf("\n\tSub Menu -> Editoras");
+								
+								printf("\n\t1 - Consultar Editora");
+								printf("\n\t2 - Cadastrar Editora");
+								printf("\n\t3 - Editar Editora");
+								printf("\n\t4 - Excluir Editora");
+								printf("\n\t5 - Sair");
+								printf("\n\tOpcão: ");	
+								scanf("%d", &menuS);
+								fflush(stdin);
+								
+								if(menuS == 1) {
+									
+								} else if (menuS == 2){
+								
+								} else if (menuS == 3) {
+									
+								} else if (menuS == 4) {
+									
+								} 
+						
+							} while (menuS != 5);
+							
+						} else if (menuP == 3) {
+							
+							do {
+								
+								system("cls");
+								printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+								printf("\n\tSub Menu -> Alunos");
+								
+								printf("\n\t1 - Consultar Aluno");
+								printf("\n\t2 - Editar Aluno");
+								printf("\n\t3 - Bloquear Aluno");
+								printf("\n\t4 - Desbloquear Aluno");
+								printf("\n\t5 - Sair");
+								printf("\n\tOpcão: ");	
+								scanf("%d", &menuS);
+								fflush(stdin);
+								
+								if(menuS == 1) {
+									
+								} else if (menuS == 2){
+								
+								} else if (menuS == 3) {
+									
+								} else if (menuS == 4) {
+									
+								} 
+						
+							} while (menuS != 5);
+								
+						} else if (menuP == 4) {
+							
+							do {
+								
+								system("cls");
+								printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+								printf("\n\tSub Menu -> Emprestimos");
+								
+								printf("\n\t1 - Consultar Emprestimo");
+								printf("\n\t2 - Realizar Emprestimo");
+								printf("\n\t3 - Prorrogar Emprestimo");
+								printf("\n\t4 - Efetuar Devolução");
+								printf("\n\t5 - Baixa de alunos com debito");
+								printf("\n\t6 - Sair");
+								printf("\n\tOpcão: ");	
+								scanf("%d", &menuS);
+								fflush(stdin);
+								
+								if(menuS == 1) {
+									
+								} else if (menuS == 2){
+								
+								} else if (menuS == 3) {
+									
+								} else if (menuS == 4) {
+									
+								} else if (menuS == 5) {
+									
+								}
+								
+							} while (menuS != 6);
+							
+						} else if (menuP == 5) {
+							
+							do {
+								
+								system("cls");
+								printf ("\n\t\t\tMENU FUNCIONARIO\n\n\n");
+								printf("\n\tSub Menu -> Relatórios");
+								
+								printf("\n\t1 - Alunos ativos");
+								printf("\n\t2 - Alunos bloquados");
+								printf("\n\t5 - Alunos com debito.");
+								printf("\n\t3 - Livros Diponíveis");
+								printf("\n\t4 - Livros Indisponíveis");
+								printf("\n\t5 - Editoras cadastradas);
+								printf("\n\t6 - Sair");
+								printf("\n\tOpcão: ");	
+								scanf("%d", &menuS);
+								fflush(stdin);
+								
+								if(menuS == 1) {
+									
+								} else if (menuS == 2){
+								
+								} else if (menuS == 3) {
+									
+								} else if (menuS == 4) {
+									
+								} else if (menuS == 5) {
+									
+								}
+								
+							} while (menuS != 6);
+							
+							
+						} 
+
+					} while (menuP != 6); 	
+					
+				} else if (saidaLogin == 3) { //Menu Para Gerente
+					
+					
+					
+				}
+			
+			} else if (login == 2) {
+			
+			
 	
+			}
+			
+		} while (login != 1 && login != 2 && login != 3);
+		
+	} while (0);
 	
-	//Funções para carregar arquivos para a memoria.
-	//carregarArquivos();
-	
-	do {
-		
-		
-		
-		
-	} while (1);
+	printf("\n\n\n\tObrigado por utilizar nosso sistema.");
 	
 }
 
